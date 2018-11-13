2dd1f85dd9a586e8ea06de40e13315bfa0d1cdf3
Dennis Wollersheim <dewoller@gmail.com>
2018-11-14 09:54:23 +1100

    With bestfile endpoint

--- lib/plumber.R
+++ lib/plumber.R
@@ -4,20 +4,20 @@ library(RMySQL)
 library(tidyverse)
 library(caret) 
 library(hms) 
-
 source('/code/pivideo/door_pix_recognize/lib/functionsMinimal.R')
+TEXT_EVENT = ''
+BEST_FILE = data.frame() 
 
-#text_event = '20181102084204'
+# load model
+# this path would have to be adapted if you would deploy this
+load("/code/pivideo/door_pix_recognize/data/predictionModel.rdata")
+
 
 # set API title and description to show up in http://localhost:8000/__swagger__/
 
 #' @apiTitle Run predictions for person prediction with logitboost model
 #' @apiDescription This API takes a text_event id, looks up parameters, runs a logitboost model on them
 #' indicate if person or not 
-
-# load model
-# this path would have to be adapted if you would deploy this
-load("/code/pivideo/door_pix_recognize/data/predictionModel.rdata")
 
 #' Log system time, request method and HTTP user agent of the incoming request
 #' @filter logger
@@ -39,10 +39,11 @@ function(req){
 #' @get /predict
 #' @html
 #' @response 200 Returns the class of text_event (person, bike, blank, car, carperson)
-calculate_prediction <- function( text_event ) {  
+calculate_prediction <<- function( text_event ) {  
 
     my_db_get_query( "select * from security where text_event=?", text_event) %>% 
     as.tibble() %>% 
+    filter( file_type==1 ) %>%
     mutate( ts = as.hms(strptime(event_time_stamp , "%Y-%m-%d %H:%M:%S"))) %>% 
     group_by( text_event ) %>%
     mutate( ms = round( frame / (max( frame ) + 1) * 10000,0),  
@@ -55,22 +56,46 @@ calculate_prediction <- function( text_event ) {
            max_width=max( motion_width ),
            max_pixel = max( numpixel )
            ) %>%
-    ungroup() %>%
-    select( numpixel:motion_Y, duration:max_pixel)  %>% 
+    ungroup() %>% 
+    select( filename, numpixel:motion_Y, duration:max_pixel)  %>% 
     distinct() %>%
-    { . } -> motion_n
+    { . } -> df_motion
 
-
-  # predict and return result
-  predict(predictionModel, motion_n) %>%
-    table() %>%
+  df_motion %>%
+    select( numpixel:motion_Y, duration:max_pixel)  %>% 
+    predict(predictionModel, newdata=., type='prob') %>%
     data.frame() %>%
-    rename(category='.') %>%
-    filter( Freq==max(Freq )) %>%
-    head(1) %>% 
+    bind_cols( filename=df_motion$filename) %>% 
+    { . } -> predictions
+
+
+  predictions %>%
+    gather( category, value, -filename ) %>%
+    group_by( filename ) %>%
+    filter( value == max( value )) %>%
+    ungroup() %>%
+    count( category, sort=TRUE ) %>%
+    head( 1 ) %>% 
     { . } -> rv
 
-    as.character(rv$category)
+  predictions %>%
+    gather( category, value, -filename ) %>%
+    filter( category == rv$category) %>%
+    arrange( desc( value )) %>%
+    head(1 ) %>% 
+    { . } ->> BEST_FILE
 
+    as.character(rv$category)
 }
 
+#' bestfile return the best file for this text_event
+#' @param text_event the text_event
+#' @get /bestfile
+#' @html
+#' @response 200 Returns the best file for this text_event
+best_file <<- function( text_event ) {  
+  if (text_event != TEXT_EVENT ) {
+    a=calculate_prediction ( text_event ) 
+  }
+  BEST_FILE$filename
+} 
