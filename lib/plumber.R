73ed8d59b13130b319a941c838f50b67d6a402c9
Dennis Wollersheim <dewoller@gmail.com>
2018-11-14 07:42:56 +1100

    plumber working

--- lib/plumber.R
+++ lib/plumber.R
@@ -5,7 +5,9 @@ library(tidyverse)
 library(caret) 
 library(hms) 
 
-source('lib/functionsMinimal.R')
+source('/code/pivideo/door_pix_recognize/lib/functionsMinimal.R')
+
+#text_event = '20181102084204'
 
 # set API title and description to show up in http://localhost:8000/__swagger__/
 
@@ -15,7 +17,7 @@ source('lib/functionsMinimal.R')
 
 # load model
 # this path would have to be adapted if you would deploy this
-load("/code/pivideo/categorisation/data/savedModel.rdata")
+load("/code/pivideo/door_pix_recognize/data/predictionModel.rdata")
 
 #' Log system time, request method and HTTP user agent of the incoming request
 #' @filter logger
@@ -33,36 +35,42 @@ function(req){
 # specify 200 (okay) return
 
 #' predict person given text_event id
-#' @param text_event:string the text_event
+#' @param text_event the text_event
 #' @get /predict
 #' @html
 #' @response 200 Returns the class of text_event (person, bike, blank, car, carperson)
-
-text_event = '20181102084204'
-
-calculate_prediction <- function( text_event ) { rloa
+calculate_prediction <- function( text_event ) {  
 
     my_db_get_query( "select * from security where text_event=?", text_event) %>% 
     as.tibble() %>% 
     mutate( ts = as.hms(strptime(event_time_stamp , "%Y-%m-%d %H:%M:%S"))) %>% 
-    { . } -> df_motion
+    group_by( text_event ) %>%
+    mutate( ms = round( frame / (max( frame ) + 1) * 10000,0),  
+           ts=as.POSIXct( paste0( event_time_stamp, '.', ms))) %>%
+    select(-ms) %>%
+    mutate( duration = as.numeric(max( ts ) - min(ts)), 
+           avg_velocity_X = (max( motion_X ) - min( motion_X )) / duration,
+           avg_velocity_Y = (max( motion_Y ) - min( motion_Y )) / duration,
+           max_height=max( motion_height ), 
+           max_width=max( motion_width ),
+           max_pixel = max( numpixel )
+           ) %>%
+    ungroup() %>%
+    select( numpixel:motion_Y, duration:max_pixel)  %>% 
+    distinct() %>%
+    { . } -> motion_n
 
-  input_data <<- as.data.frame(cbind(input_data_num, input_data_int))
-
-  # validation for parameter
-  if (any(is.na(input_data))) {
-    res$status <- 400
-    res$body <- "Parameters have to be numeric or integers"
-  }
-
-#  if (any(input_data < 0) || any(input_data > 1)) {
-#    res$status <- 400
-#    res$body <- "Parameters have to be between 0 and 1"
-#  }
 
   # predict and return result
-  pred_rf <<- predict(model_rf, input_data)
-  as.character(pred_rf)
-}
+  predict(predictionModel, motion_n) %>%
+    table() %>%
+    data.frame() %>%
+    rename(category='.') %>%
+    filter( Freq==max(Freq )) %>%
+    head(1) %>% 
+    { . } -> rv
 
+    as.character(rv$category)
+
+}
 
